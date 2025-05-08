46d5aa26d37ec57a9e9fa308e7cae539c2efc5b8
Alef-Henrique <alefsax7@outlook.com>
2025-05-08 10:48:37 -0300

    Atualização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { v4 as uuid } from "uuid"
 
-import { Container, ToDoList, Imput, Button} from './styles'
+
+import { Container, ToDoList, Imput, Button, ListItem, Trash, Check, TextoVazio } from './styles'
 
 function App() {
-  const [list, setList] = useState([{id: uuid(), task: 'Nada'}])
+  const [list, setList] = useState([])
   const [inputTask, setInputTask] = useState('')
 
 
@@ -13,22 +14,47 @@ function App() {
   }
 
   function clicouNoBotao() {
-    setList([ ...list, {id: uuid(), task: inputTask}])
+    if (inputTask.trim() === '') return
+    setList([...list, { id: uuid(), task: inputTask, finished: false }])
+    setInputTask('')
+  }
+  function finalizarTarefa(id) {
+
+    const newList = list.map(item => (
+      item.id === id ? { ...item, finished: !item.finished } : item
+    ))
+    setList(newList)
+  }
+  function deletarItem(id) {
+    const newList = list.filter(item => item.id !== id)
+    setList(newList)
   }
 
   return (
     <Container>
       <ToDoList>
-      <Imput onChange={inputMudou} placeholder="O que tenho para fazer..." />
-      <Button onClick={clicouNoBotao}>Adicionar</Button>
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>{item.task}</li>
-        ))}
-      </ul>
+        <Imput onChange={inputMudou} placeholder="O que tenho para fazer..." />
+        <Button onClick={clicouNoBotao}>Adicionar</Button>
+        <ul>
+
+          {
+            list.length > 0 ? (
+              list.map((item) => (
+                <ListItem $isFinished={item.finished} key={item.id}>
+                  <Check onClick={() => finalizarTarefa(item.id)} />
+                  <li>{item.task}</li>
+                  <Trash onClick={() => deletarItem(item.id)} />
+                </ListItem>
+
+              ))
+          ) : (
+            <TextoVazio>Sem itens na lista</TextoVazio>
+          )
+        }
+        </ul>
       </ToDoList>
     </Container>
-    
+
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FcOk, FcEmptyTrash } from "react-icons/fc";
 
 
 export const Container = styled.div`
@@ -14,6 +15,12 @@ export const ToDoList = styled.div`
     background-color: white;
     padding: 30px 20px;
     border-radius: 5px;
+    width: 500px;
+    margin: 20px;
+    ul{
+        padding: 0;
+        margin-top: 60px;
+    }
 
 `
 export const Imput = styled.input`
@@ -21,6 +28,8 @@ export const Imput = styled.input`
     border-radius: 5px;
     margin-right: 10px;
     height: 30px;
+    width: 300px;
+    margin-right: 20px;
 `
 export const Button = styled.button`
     background: #8052ec;
@@ -30,5 +39,39 @@ export const Button = styled.button`
     height: 30px;
     border: none;
     color: #ffffff;
+    cursor: pointer;
+    &:hover{
+        opacity: 0.8;
+    }
+    &:active{
+        opacity: 0.6;
+    }
+`
+export const ListItem = styled.div`
+    background: ${props => (props.$isFinished ? '#E8FF8B' : '#E4E4E4')};
+    box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 5px;
+    margin-bottom: 20px;
+    width: 100%;
 
+    li{
+        list-style-type: none;
+    }
+
+`
+export const Trash = styled(FcEmptyTrash)`
+    cursor: pointer;
+`
+export const Check = styled(FcOk)`
+    cursor: pointer;
+`
+export const TextoVazio = styled.h3`
+
+    text-align: center;
+    color: gray;
 `
